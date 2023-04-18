bcc0c845f9e7f8da922afcae4385abd38c614284
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2023-04-18 11:43:52 -0300

    fix: excluido arquivos nao utilizados

--- src/lib/consulta/extrair_logs/Extrair_Logs.d.ts
+++
@@ -1,2 +0,0 @@
-import { ModelExtrairLogs } from '../../model/consulta/extrair_logs/Extrair_Logs';
-export declare const extrairLogs: (_numeroSessao: number, _codigoAtivacao: string) => Promise<ModelExtrairLogs>;
