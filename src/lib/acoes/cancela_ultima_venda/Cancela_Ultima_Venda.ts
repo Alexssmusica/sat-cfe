4d7ccb2dc53fabf5964b0c069723c460cfef35cb
Alex Santos de Souza <alexssmusica@gmail.com>
2023-07-07 07:46:19 -0300

    removido console de teste

--- src/lib/acoes/cancela_ultima_venda/Cancela_Ultima_Venda.ts
+++ src/lib/acoes/cancela_ultima_venda/Cancela_Ultima_Venda.ts
@@ -25,7 +25,6 @@ export const cancelarUltimaVenda: (
 					if (error) {
 						throw new Error(error);
 					}
-					console.log(resultCancelarUltimaVenda, 'resultCancelarUltimaVenda');
 					resultCancelarUltimaVenda = UTF8.decode(resultCancelarUltimaVenda as string);
 					const _cancelarUltimaVenda = new ModelCancelarUltimaVenda();
 					_cancelarUltimaVenda.fromArray(resultCancelarUltimaVenda.split('|'));
@@ -60,7 +59,6 @@ export const cancelarUltimaVendaEmulador: (
 					if (error) {
 						throw new Error(error);
 					}
-					console.log(resultCancelarUltimaVenda, 'resultCancelarUltimaVenda');
 					resultCancelarUltimaVenda = UTF8.decode(resultCancelarUltimaVenda as string);
 					const _cancelarUltimaVenda = new ModelCancelarUltimaVenda();
 					_cancelarUltimaVenda.fromArray(resultCancelarUltimaVenda.split('|'));
